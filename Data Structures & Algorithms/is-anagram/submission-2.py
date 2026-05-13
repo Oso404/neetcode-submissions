4727e10c6281b2ac9385d41cabf1ea586e69215b
Oso404 <joules.programming.8122@gmail.com>
2026-05-13 09:20:34 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hm1 = dict()
+        hm2 = dict()
+        for i in range(len(s)):
+            if s[i] not in hm1:
+                hm1[s[i]] = 1
+            else:
+                hm1[s[i]] += 1
+            if t[i] not in hm2:
+                hm2[t[i]] = 1
+            else:
+                hm2[t[i]] += 1
+        return hm1 == hm2
+
+        
