0c8667f3d508d1aacbd0201c5d44df4bcecb3242
Oso404 <joules.programming.8122@gmail.com>
2026-05-21 08:50:13 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,26 @@
+"""
+similar to valid anagrams problem
+we used the ordinance technique (26 0-value array)
+dictionary: {
+    (1,0,1...):["ac..."],
+    ....
+}
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        store = dict()
+        grouped = [[]]
+        #first solution i frequency counter...will do again 
+        for word in strs:
+            lst = [0] * 26
+            for letter in word:
+                ind = ord(letter.lower()) - ord('a')
+                lst[ind] += 1
+            tl = tuple(lst)
+            if tl not in store:
+                store[tl] = [word]
+            else:
+                store[tl].append(word)
+        
+
+        return list(store.values())
