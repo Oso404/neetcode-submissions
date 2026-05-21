67c4ac4539dc2286b5c162a321e07a12ef5dc8c8
Oso404 <joules.programming.8122@gmail.com>
2026-05-21 09:10:04 -0400

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,23 @@
+"""
+two pointer algo
+suppose we have two heights: 3 and 9....we want to keep 9 and not 3...moving away from 9 would reduce width and height
+key: move pointer with smallest height
+keywords: "max area" --> giveaway for two pointers algorithm
+the closer we move inward the taller we want the bar to be to get the most area
+"""
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l = 0
+        r = len(heights)-1
+        ar = 0
+        while l < r:
+            if(heights[l] < heights[r]):
+                ca = (r-l) * (heights[l])
+                l+=1
+            else: #both heights are equal or r is smaller
+                ca = (r-l) * (heights[r])
+                r-=1
+            ar = max(ar,ca)
+        return ar
+        
+        
