29aa0195f57e5dcec9909c7d5946ad169f4f501e
Oso404 <joules.programming.8122@gmail.com>
2026-05-21 08:46:47 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,12 @@
+"""
+classic dictionary example
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = dict()
+        for index,num in enumerate(nums):
+            if target - num not in store:
+                store[num] = index
+            else:
+                return [store[target-num],index]
+        
